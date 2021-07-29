540810b9ef6611f09a687392566a4ed9a0e33c0b
ayush-pandey007 <70948759+ayush-pandey007@users.noreply.github.com>
2021-07-29 22:28:10 +0530

    completed

---
+++ day-25/src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import "tailwindcss/tailwind.css";
+
+function App() {
+  return (
+    <div className="App flex flex-col justify-center items-center h-screen">
+      <section className="container h-2/5 w-1/2 rounded-lg p-5 shadow-md bg-white">
+        <h1 className="font-bold text-green-400 text-2xl text-left">Hello</h1>
+        <p className="text-left  text-gray-400 text-lg">i am a AYUSH PANDEY</p>
+      </section>
+      <p className="mt-9  text-blue-300">THANK YOU</p>
+    </div>
+  );
+}
+
+export default App;
